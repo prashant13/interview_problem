90d826354b38d2c9226eec1dce58dee23f0b105a
Prashant <prashantr.1311@gmail.com>
2015-01-08 19:10:04 +0530

    db migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150108130047) do
+ActiveRecord::Schema.define(version: 20150108133615) do
+
+  create_table "comments", force: true do |t|
+    t.string   "context"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "likes", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "stories", force: true do |t|
+    t.string   "title"
+    t.string   "context"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
